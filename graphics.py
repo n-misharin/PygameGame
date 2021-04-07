38396552358316a059aabecba1d23a9a0a043f34
nikita <misharinnikita@gmail.com>
2021-04-07 03:03:36 +0300

    Add graphics.py, begin pygame GUI, add images

--- graphics.py
+++ graphics.py
@@ -1,12 +1,78 @@
 import pygame
+import os
+
+pygame.init()
+
+pygame.display.set_caption("Game")
+
+size = width, height = 800, 800
+screen = pygame.display.set_mode(size)
+
+background = pygame.Surface(size)
+background.fill(pygame.Color('#000000'))
 
 
-class SpriteSheet:
-    def __init__(self, image_name):
-        self.texture = pygame.image.load(image_name)
+class ImageHandler:
 
-    def get_sub(self, x, y):
-        return pygame.transform.smoothscale(self.texture, )
+    DEFAULT_COLORKEY = -1
+
+    def __init__(self, path="images"):
+        self.path = path
+
+    def load_image(self, image_name, colorkey=None):
+        fullname = os.path.join(self.path, image_name)
+        if not os.path.isfile(fullname):
+            raise Exception(f"Файл {fullname} не найден")
+            #return None
+        else:
+            return self.convert_alpha(pygame.image.load(fullname), colorkey)
+
+    def convert_alpha(self, image, colorkey=None):
+        if colorkey is not None:
+            image = image.convert()
+            if colorkey == self.DEFAULT_COLORKEY:
+                colorkey = image.get_at((0, 0))
+            image.set_colorkey(colorkey)
+        else:
+            image = image.convert_alpha()
+        return image
 
 
-class
+class AnimatedSprite(pygame.sprite.Sprite):
+
+    FRAME_DURATION = 1
+
+    def __init__(self, sheet, columns, rows, x, y):
+        super().__init__()
+        self.frames = []
+        self.cur_frame_time = 0
+        self.cut_sheet(sheet, columns, rows)
+        self.cur_frame_x = 0
+        self.cur_frame_y = 0
+        self.image = self.frames[self.cur_frame_y][self.cur_frame_y]
+        self.rect = self.rect.move(x, y)
+
+    def cut_sheet(self, sheet, columns, rows):
+        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns, sheet.get_height() // rows)
+
+        for j in range(rows):
+            self.frames.append([])
+            for i in range(columns):
+                frame_location = (self.rect.w * i, self.rect.h * j)
+                self.frames[j].append(sheet.subsurface(pygame.Rect(frame_location, self.rect.size)))
+
+    def update(self, delta_time=0.001):
+        self.cur_frame_time += delta_time
+        if self.cur_frame_time >= self.FRAME_DURATION:
+            self.cur_frame_time = 0
+            self.cur_frame_x = (self.cur_frame_x + 1) % len(self.frames[self.cur_frame_y])
+            self.image = self.frames[self.cur_frame_y][self.cur_frame_x]
+
+    def set_frame_y(self, y):
+        self.cur_frame_y = y
+        self.cur_frame_x = 0
+
+
+class SpriteGroup(pygame.sprite.Group):
+    def draw_on_screen(self):
+        self.draw(screen)
--- main.py
+++ main.py
@@ -1,21 +1,83 @@
+import graphics
 import pygame
-import random
-from Map import Field, Board, FieldTypes, get_field_types, FieldsProperties
-from Units import Unit
+import pygame_gui
+from Map import Board, FieldTypes
+
+
+def board_draw(_board: Board):
+    tile_size = 50
+    board_sprites = graphics.SpriteGroup()
+    for j in range(len(_board._fields)):
+        line = _board._fields[j]
+        for i in range(len(line)):
+            field = line[i]
+            field_sprite = pygame.sprite.Sprite()
+
+            file_name = "soil"
+            if field.type == FieldTypes.OIL:
+                file_name = "oil"
+            elif field.type == FieldTypes.DIAMOND:
+                file_name = "diamond"
+            elif field.type == FieldTypes.GOLD:
+                file_name = "gold"
+            elif field.type == FieldTypes.WATER:
+                file_name = "water"
+            elif field.type == FieldTypes.STONE:
+                file_name = "stone"
+            elif field.type == FieldTypes.TUNNEL:
+                file_name = "soil"
+
+            field_sprite.image = graphics.ImageHandler().load_image(file_name + ".png")
+            field_sprite.rect = field_sprite.image.get_rect()
+            field_sprite.rect.x = tile_size * i
+            field_sprite.rect.y = tile_size * j
+            board_sprites.add(field_sprite)
+    board_sprites.draw_on_screen()
 
 
 if __name__ == '__main__':
+
+    window_surface = pygame.display.set_mode(graphics.size)
+    manager = pygame_gui.UIManager(graphics.size)
+
+    hello_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((350, 275), (100, 50)),
+                                                text='Say Hello',
+                                                manager=manager)
+    unit_panel = pygame_gui.elements.UIPanel(relative_rect=pygame.Rect((600, 0), (200, 200)),
+                                             manager=manager,
+                                             starting_layer_height=1)
+    unit_type_label = pygame_gui.elements.UILabel(relative_rect=pygame.Rect((650, 30), (50, 60)),
+                                                  manager=manager, parent_element=unit_panel,
+                                                  text="LOL")
+
     board = Board.get_random((10, 10))
 
-    # pygame.init()
-    # size = width, height = 800, 400
-    # screen = pygame.display.set_mode(size)
-    #
-    # running = True
-    # while running:
-    #     for event in pygame.event.get():
-    #         if event.type == pygame.QUIT:
-    #             running = False
-    #
-    #     pygame.display.flip()
-    # pygame.quit()
+    clock = pygame.time.Clock()
+    running = True
+
+    count = 0
+
+    while running:
+        delta_time = clock.tick(60) / 1000
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+            if event.type == pygame.USEREVENT:
+                if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+                    if event.ui_element == hello_button:
+                        print('Hello World!')
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                count += 1
+                print(count)
+                unit_type_label.set_text(str(count))
+
+            manager.process_events(event)
+
+        manager.update(delta_time)
+        board_draw(board)
+        manager.draw_ui(window_surface)
+
+        pygame.display.flip()
+    pygame.quit()
